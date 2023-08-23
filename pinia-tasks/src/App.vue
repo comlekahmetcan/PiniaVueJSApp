e9a5f996dcf79eae2495abc10c9ab64020db2196
comlekahmetcan <ahmetcan.comlek@cts.com.tr>
2023-08-23 11:18:14 +0300

    for döngüsü ile store 'da ki tasks ulaşışldı

--- pinia-tasks/src/App.vue
+++ pinia-tasks/src/App.vue
@@ -3,8 +3,16 @@
     <!-- heading -->
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
-      <h1>{{ taskStore.name }}</h1>
+      <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- task list -->
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <p>{{ task.title }}</p>
+      </div>
+    </div>
   </main>
 </template>
 
