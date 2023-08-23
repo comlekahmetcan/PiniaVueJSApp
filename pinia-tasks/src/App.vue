d4699b50be2febace0cf7011a7bcb62ac0f6c5bc
comlekahmetcan <ahmetcan.comlek@cts.com.tr>
2023-08-23 12:20:29 +0300

    Task listesinde store için getters methodunu kullanıldı, favori ve tüm task listesi birbirinden ayrıldı

--- pinia-tasks/src/App.vue
+++ pinia-tasks/src/App.vue
@@ -6,10 +6,24 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
     <!-- task list -->
 
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>Your have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>Your have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -17,7 +31,9 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./stores/TaskStore";
-const taskStore = useTaskStore();
+const taskStore = useTaskStore(),
+  filter = ref("all");
 </script>
