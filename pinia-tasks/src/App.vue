a75305cc3c093c2707c42eef2b2102b001d56955
comlekahmetcan <ahmetcan.comlek@cts.com.tr>
2023-08-23 10:53:13 +0300

    Başlangıç için düzenlemeler yapıldı

--- pinia-tasks/src/App.vue
+++ pinia-tasks/src/App.vue
@@ -1,4 +1,18 @@
 <template>
-  
+  <main>
+
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" >
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    
+  </main>
 </template>
 
+<script>
+  export default {
+    
+  }
+</script>
