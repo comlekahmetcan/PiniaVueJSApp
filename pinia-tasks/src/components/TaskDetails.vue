18b334bcfe3eb8ac53af1c1f476fe20b78348b5b
comlekahmetcan <ahmetcan.comlek@cts.com.tr>
2023-08-23 13:00:20 +0300

    task silme ve favorilere ekleme store üzerinde düzenlendi ve detay sayfasına eklendi

--- pinia-tasks/src/components/TaskDetails.vue
+++ pinia-tasks/src/components/TaskDetails.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { useTaskStore } from "../stores/TaskStore";
+
+const taskStore = useTaskStore();
+
 const props = defineProps({
   task: {
     type: Array,
@@ -9,12 +13,21 @@ const props = defineProps({
 </script>
 
 <template>
-    
   <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
-      <span class="material-symbols-outlined icon"> delete </span>
-      <span class="material-symbols-outlined icon"> favorite </span>
+      <span
+        class="material-symbols-outlined icon"
+        @click="taskStore.deleteTask(task.id)"
+      >
+        delete
+      </span>
+      <span
+        class="material-symbols-outlined icon"
+        @click="taskStore.toogleFav(task.id)"
+      >
+        favorite
+      </span>
     </div>
   </div>
 </template>
