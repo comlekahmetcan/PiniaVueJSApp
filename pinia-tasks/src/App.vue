598139fb5ecb6785fc6a3fd3b8e1999ce48b3388
comlekahmetcan <ahmetcan.comlek@cts.com.tr>
2023-08-23 11:12:52 +0300

    Task store 'dan name bilgisi okundu

--- pinia-tasks/src/App.vue
+++ pinia-tasks/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <main>
-
     <!-- heading -->
     <header>
-      <img src="./assets/pinia-logo.svg" alt="pinia logo" >
-      <h1>Pinia Tasks</h1>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>{{ taskStore.name }}</h1>
     </header>
-
-    
   </main>
 </template>
 
-<script>
-  export default {
-    
-  }
+<script setup>
+import { useTaskStore } from "./stores/TaskStore";
+const taskStore = useTaskStore();
 </script>
